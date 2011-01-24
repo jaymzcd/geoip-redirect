95039511cbdc2daf20f301f309aa99511f13d1b5
jaymz <jaymz@u-dox.com>
2011-01-24 17:06:54 +0000

    split templates, add in debug var to force ip. some changes to logic

--- geoip/middleware.py
+++ geoip/middleware.py
@@ -1,11 +1,17 @@
 from django.template.loader import render_to_string
 from geoip.models import GeoIPRecord, IPRedirectEntry
+from geoip.conf import geo_setting
+from django.conf import settings
 from django.utils.encoding import smart_str
 
 class GeoIPMiddleWare(object):
 
     def __init__(self):
-        self.redirect_inject = render_to_string('geoip/redirect.html')
+        self.redirect_inject = render_to_string('geoip/base_redirect.html')
+        if geo_setting('DEBUG_IP'):
+            self.DEBUG_IP = geo_setting('DEBUG_IP')
+        else:
+            self.DEBUG_IP = False
 
     def process_response(self, request, response):
         """ Read in the users IP address from the request object. If we
@@ -13,12 +19,28 @@ class GeoIPMiddleWare(object):
         active redirect then inject the HTML into the template to show
         a lightbox to the user """
 
-        inbound_ip = request.META['REMOTE_ADDR']
-        user_code = GeoIPRecord.get_code(inbound_ip)
-        redirect_list = IPRedirectEntry.objects.all().values_list('incoming_country_code', flat=True)
+        # The following is for inplace debugging forcing a particular IP
+        if settings.DEBUG and self.DEBUG_IP:
+            inbound_ip = self.DEBUG_IP
+        else:
+            inbound_ip = request.META['REMOTE_ADDR']
 
-        if user_code in redirect_list and r'/admin' not in request.path:
+        user_code = GeoIPRecord.get_code(inbound_ip)
+        redirect_list = IPRedirectEntry.objects.all()
+
+        ccodes = redirect_list.values_list('incoming_country_code', flat=True)
+        if user_code in ccodes and r'/admin' not in request.path:
             # Slight hack above to avoid hooking into admin
+            code_index = list(ccodes).index(user_code) # use this to grab the template data
+            redirect_data = redirect_list[code_index] # in theory this is the right one
+            context = dict(redirect=redirect_data)
+
+            if redirect_data.custom_message is not None:
+                inject_data = render_to_string('geoip/custom_redirect.html', context)
+            else:
+                inject_data = self.redirect_inject # use one from __init__ time
+
             response.content = smart_str(response.content) + \
-                smart_str(self.redirect_inject)
+                smart_str(inject_data)
+
         return response
--- geoip/settings.py
+++ geoip/settings.py
@@ -1,1 +1,2 @@
 FAIL_ON_MISSING = False # If we want to raise Exceptions when no code was found
+DEBUG_IP = '92.52.69.82' # Set this if you want to force it to "find" this particular address
