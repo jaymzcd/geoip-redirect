ea6591513bdd130eed6c938f7682f74e3dd36484
jaymz <jaymz@u-dox.com>
2011-01-24 17:57:34 +0000

    adding some stuff TODO

--- geoip/settings.py
+++ geoip/settings.py
@@ -3,5 +3,7 @@ REDIRECT_DOMAIN = 'http://www.google.com' # The domain to redirect to if REDIREC
 REDIRECT_CODES = ['GB', 'FR', 'DE']
 
 FAIL_ON_MISSING = False # If we want to raise Exceptions when no code was found
+SHOW_FOR_MISSING = True # If true then we should pop up an option anyway
+MISSING_DOMAIN = 'http://www.vans.com/intl/' # Target URL when we have no idea where they are
 
 DEBUG_IP = '62.149.188.154' # Set this if you want to force it to "find" this particular address
