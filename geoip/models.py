6be17895030758ad21f873f8d0eb4b3a95943230
jaymz <jaymz@u-dox.com>
2011-01-24 11:57:32 +0000

    add readme, initial model. tweak settings

--- geoip/models.py
+++ geoip/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class GeoIPRecord(models.Model):
+    """ The source data for this table can be found at
+    http://www.maxmind.com/app/geoip_country. Fields are as they are from
+    that file so load in via the following sql once you've ran syncdb:
+
+    """
+
+    start_ip = models.IPAddressField()
+    end_ip = models.IPAddressField()
+    start_decimal = models.IntegerField()
+    end_decimal = models.IntegerField()
+    country_code = models.CharField(max_length=2)
+    country_name = models.CharField(max_length=25)
+
+    def __unicode__(self):
+        return '%s - %s [%s]' % (self.start_ip, self.end_ip, self.country_code)
+
--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 # Django settings for geoipredirect project.
+import os
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,8 +13,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_PATH, 'db.sqlite3'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -89,6 +91,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'geoip',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
