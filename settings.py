c5c9cb96fe6ba4587cd42419ae7260e3c0abe3b0
unknown <Nina@.(none)>
2011-02-06 12:16:16 +0000

    fixes to run dummy app

--- settings.py
+++ settings.py
@@ -83,6 +83,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -93,7 +94,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'geoip',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
