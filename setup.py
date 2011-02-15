676c48f665876d5d52f267d29c118475e09235d1
jaymz <jaymz@u-dox.com>
2011-02-15 12:02:03 +0000

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='geoipredirect',
+      version='0.2.0',
+      description='Django middleware to insert a redirect lightbox based on users IP',
+      author='Jaymz Campbell',
+      author_email='jaymz@jaymz.eu',
+      url='https://github.com/jaymzcd/geoip-redirect',
+      packages=['geoip',],
+     )
